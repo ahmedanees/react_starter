05b0f3e44312a0d57e288950a2017d6d5ea2c361
Ahmed Umer Anees <ahmed.umer.anees@gmail.com>
2021-02-08 23:13:21 +0500

    add navigation into latest project

---
+++ src/screens/LoginScreen/LoginContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginView from "./LoginView";
+
+const LoginContainer = () => {
+    return (
+       <LoginView  />
+    );
+}
+export default LoginContainer;
---
+++ src/screens/RegistrationScreen/RegisterContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import RegisterView from "./RegisterView";
+
+const RegisterContainer = () => {
+    return (
+       <RegisterView  />
+    );
+}
+export default RegisterContainer;
